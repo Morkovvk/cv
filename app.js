10ac8251d50438e685667e209d48ad1497162ca4
Morkovvk <kononchuk27@gmail.com>
2023-07-22 03:11:42 +0300

    added the ability to add projects

--- app.js
+++ app.js
@@ -6,7 +6,36 @@ const app = Vue.createApp({
             position: '',
             descr: '',
 
+            projects: [],
+            newProject: {
+                name: '',
+                description: '',
+                url: '',
+            },
+
         };
+    },
+    methods: {
+        addProject() {
+            // Перевірка на заповненість полів проекту перед додаванням
+            if (this.newProject.name && this.newProject.description && this.newProject.url) {
+                // Генеруємо унікальний ідентифікатор для нового проекту
+                const newId = this.projects.length + 1;
+                this.newProject.id = newId;
+
+                // Додаємо новий проект до масиву projects
+                this.projects.push(this.newProject);
+
+                // Очищаємо дані з форми
+                this.newProject = {
+                    name: '',
+                    description: '',
+                    url: '',
+                };
+            } else {
+                alert('Будь ласка, заповніть всі поля проекту.');
+            }
+        },
     },
 });
 
